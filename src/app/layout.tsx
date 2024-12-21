dd024d42ff2760bc3cab76f03aaf7ffa67fc2a5d
alexbaumgertner <alex.baumgertner@gmail.com>
2024-12-22 00:00:51 +0300

    Put back content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,14 @@
 import { SessionProvider } from 'next-auth/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Analytics } from '@vercel/analytics/next'
+import { Container, Row, Col, Breadcrumb, Nav } from 'react-bootstrap'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
 
 import { ClientProvider } from '@/components/Apollo'
 import { Footer } from '@/components'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Container, Row, Col, Breadcrumb } from 'react-bootstrap'
-import { usePathname } from 'next/navigation'
 
 export default function RootLayout({
   children,
@@ -44,7 +45,36 @@ export default function RootLayout({
         <SessionProvider>
           <Container>
             <Row>
-              <Col>
+              <Col md={2}>
+                <Nav className="flex-column">
+                  <Nav.Item>
+                    <Link href="/" passHref legacyBehavior>
+                      <Nav.Link>Главная</Nav.Link>
+                    </Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Link href="/diagnostics" passHref legacyBehavior>
+                      <Nav.Link>Диагностика</Nav.Link>
+                    </Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Link href="/treatment" passHref legacyBehavior>
+                      <Nav.Link>Лечение</Nav.Link>
+                    </Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Link href="/life-style" passHref legacyBehavior>
+                      <Nav.Link>Образ жизни</Nav.Link>
+                    </Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Link href="/news" passHref legacyBehavior>
+                      <Nav.Link>Новости</Nav.Link>
+                    </Link>
+                  </Nav.Item>
+                </Nav>
+              </Col>
+              <Col md={10}>
                 <ClientProvider>
                   {children}
                   <SpeedInsights />
