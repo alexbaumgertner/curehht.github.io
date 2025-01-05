047d28d5ed87aa9807967bff317daf7021b83549
alexbaumgertner <alex.baumgertner@gmail.com>
2025-01-05 20:30:40 +0300

    Enable pages

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,60 +1,55 @@
-// import gql from 'graphql-tag'
+import gql from 'graphql-tag'
 import { notFound } from 'next/navigation'
 
-// import { getClient } from '@/components/Apollo/ServerProvider'
+import { getClient } from '@/components/Apollo/ServerProvider'
 
-// const GET_PAGES_SLUG = gql`
-//   query GetPages {
-//     pages {
-//       slug
-//     }
-//   }
-// `
-// const GET_PAGE = gql`
-//   query GetPage($slug: String!) {
-//     page(slug: $slug) {
-//       id
-//       slug
-//       title
-//       summary
-//       content
-//       created_at
-//       updated_at
-//     }
-//   }
-// `
+const GET_PAGES_SLUG = gql`
+  query GetPages {
+    pages {
+      slug
+    }
+  }
+`
+const GET_PAGE = gql`
+  query GetPage($slug: String!) {
+    page(slug: $slug) {
+      id
+      slug
+      title
+      summary
+      content
+      created_at
+      updated_at
+    }
+  }
+`
 
-// async function fetchSlugsFromDB() {
-//   // const client = getClient()
-//   //   const { data } = await client.query({
-//   //     query: GET_PAGES_SLUG,
-//   //   })
+async function fetchSlugsFromDB() {
+  const client = getClient()
+  const { data } = await client.query({
+    query: GET_PAGES_SLUG,
+  })
 
-//   // return data.pages.map((page: { slug: string }) => page.slug)
-//   return []
-// }
+  return data.pages.map((page: { slug: string }) => page.slug)
+}
 
 export async function generateStaticParams() {
-  //const slugs = await fetchSlugsFromDB()
+  const slugs = await fetchSlugsFromDB()
 
-  // return slugs.map((slug: string) => ({
-  //   slug,
-  // }))
-
-  return []
+  return slugs.map((slug: string) => ({
+    slug,
+  }))
 }
 
 async function fetchPageBySlug(slug: string) {
-  // const client = getClient()
-  // const { data } = await client.query({
-  //   query: GET_PAGE,
-  //   variables: {
-  //     slug,
-  //   },
-  // })
-  // return data.page
-  console.log('_______fetchPageBySlug', { slug })
-  return null
+  const client = getClient()
+  const { data } = await client.query({
+    query: GET_PAGE,
+    variables: {
+      slug,
+    },
+  })
+  return data.page
 }
 
 export default async function SlugPage({ params }) {
